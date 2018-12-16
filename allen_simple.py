be1faa5e518498fdf6d810d8c549cd3a179c48ef
isaacmg <igodfried@isaac26.com>
2018-12-15 23:44:09 -0500

    add

--- allen_simple.py
+++ allen_simple.py
@@ -1,17 +1,27 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+
 import allennlp
 from allennlp.predictors.predictor import Predictor
 import json 
-
+import os 
 app = Flask(__name__)
 api = Api(app)
 
 class AllenWraper(Resource):
-    def __init__(self):
-        self.predictor = Predictor.from_path("url")
+   def __init__(self, url: str):
+        self.predictor = Predictor.from_path(url)
+        self.token = os.environ["token"]
+        
+    def predict(user_string: str):
+        return self.predictor(user_string) 
+        
     def post(self):
-        json.load(request.data)
+        data = json.load(request.data)
+        if data["token"] == token:
+            pass
+        
+        
         
 
 api.add_resource(AllenWraper, '/get_model') 
