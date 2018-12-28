137b7ce6b733fd7c73c34ea47d29459ca30a7d0a
isaacmg <igodfried@isaac26.com>
2018-12-28 14:00:55 -0500

    update unit

--- tests/test_allen_ai.py
+++ tests/test_allen_ai.py
@@ -5,8 +5,8 @@ import json
 class TestFlaskAPIs(unittest.TestCase):
     def test_allen_nlp(self):
         response = requests.get("http://127.0.0.1:5050/get_model", data=json.dumps({"token":"testToken", "text":"My name is dale smith what is yours?"}))
-        print(response.text)
-        self.assertEqual(1,1)
+        # Unit test to verify response data is a (JSON) dictionary object
+        self.assertEqual(type(response.text), dict)
 
 
 if __name__ == '__main__':
