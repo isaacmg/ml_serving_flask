790bbc0985d72d60d8d26a3143045ff498fded50
Isaac Godfried <igodfried@isaac26.com>
2019-06-04 18:22:20 -0400

    add zone to file

--- kube_config/gcp_environment.sh
+++ kube_config/gcp_environment.sh
@@ -1,4 +1,4 @@
-gcloud container clusters get-credentials ml-flask-1
+gcloud container clusters get-credentials standard-cluster-1 --zone=enter_your_zone
 kubectl create -f flask-deployment.yml
 kubectl create -f flask-service.yml
 kubectl apply -f gcp-ingress.yml
